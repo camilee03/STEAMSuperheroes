5d70f733fd90c53fc1a61f2d698cef41b2730d33
camilee03 <crlee15@asu.edu>
2025-02-14 15:03:30 -0700

    Character Designs

---
+++ STEAM Superheroes Game/Assets/Scripts/Character/OutfitManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class OutfitManager : MonoBehaviour
+{
+    public Image[] shirts;
+    public Image[] pants;
+
+    // color scheme will be determined by %4 values
+    
+    enum ColorScheme
+    {
+
+    }
+
+    void CycleThroughShirts()
+    {
+
+    }
+
+    void CycleThroughPants()
+    {
+
+    }
+
+    void ChangeColorScheme()
+    {
+
+    }
+}
