7a5d749ca35b56d1082397286255c7cfa2bcf211
TylerFujikawa <tylerfujikawa@gmail.com>
2025-04-14 17:32:54 -0700

    Dialogue System

--- STEAM Superheroes Game/Assets/Scripts/Dialogue/Dialogue_SO.cs
+++ STEAM Superheroes Game/Assets/Scripts/Dialogue/Dialogue_SO.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Dialogue Slide")]
 public class Dialogue_SO : ScriptableObject {
-    public string text { get; }
-    public bool type { get; } //true is player, false is non player
+    [Header("Properties")]
+    [SerializeField] bool onLeftSide = false; //0 is left, 1 is right
+    [SerializeField] bool usesPlayerImage = false;
+    [SerializeField] Sprite speakerImage = null; //not used if usesPlayerImage is true
+    [SerializeField] Sprite extraImage = null;
+    [TextArea(15, 20)]
+    [SerializeField] string dialogueText = "";
+
+    public bool OnLeftSide => onLeftSide;
+    public string DialogueText => dialogueText;
+    public bool UsesPlayerImage => usesPlayerImage;
+    public Sprite SpeakerImage => speakerImage;
+    public Sprite ExtraImage => extraImage;
 }
