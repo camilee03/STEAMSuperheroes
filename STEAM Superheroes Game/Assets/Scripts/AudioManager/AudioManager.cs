284a1910d89709878fe3e4654faf086eddf3a736
Aturasuu <aturasu.dev@gmail.com>
2025-09-23 19:45:56 -0700

    Moved audio files to a separate scriptable object and updated audio manager

--- STEAM Superheroes Game/Assets/Scripts/AudioManager/AudioManager.cs
+++ STEAM Superheroes Game/Assets/Scripts/AudioManager/AudioManager.cs
@@ -9,50 +9,65 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
 
-    [Header("-------- Audio Clip --------")]
-    [Header("General")]
-    public AudioClip levelComplete;
-    public AudioClip buttonClick;
+    [SerializeField] private SoundData_SO soundBank;
 
-    [Header("Level Specific")]
-    public AudioClip backgroundMusic;
-
-    [Header("Art Minigame")]
-    public AudioClip shapeGrabbed;
-    public AudioClip shapeReleased;
-
-    [Header("Circuit Minigame")]
-    public AudioClip logicGateOptionsOpened;
-    public AudioClip logicGateOptionSelected;
-    public AudioClip logicGateEndSuccess;
-    public AudioClip logicGateEndFailure;
-
-    [Header("Math Minigame")]
-    public AudioClip paintSelected;
-    public AudioClip paintApplied;
-
-    [Header("Mining Minigame")]
-    public AudioClip clickHexagon;
-    public AudioClip mineralCollected;
-
-    [Header("Satellite Minigame")]
-    public AudioClip satelliteCollected;
-    public AudioClip droneMoving;
+    private AudioClip currentBGM;
 
     private void Awake()
     {
-        if (instance == null) instance = this;
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
     }
 
     private void Start()
     {
-        musicSource.clip = backgroundMusic;
+        SetBGMTrack(BgmTrack.MAINMENU);
+    }
+
+    public void SetBGMTrack(BgmTrack bgmTrack)
+    {
+        switch (bgmTrack)
+        {
+            case BgmTrack.PREMINIGAME:
+                currentBGM = soundBank.preMinigameBgm;
+                break;
+            case BgmTrack.MINING:
+                currentBGM = soundBank.miningMinigameBgm;
+                break;
+            case BgmTrack.PROGRAMMING:
+                currentBGM = soundBank.programmingMinigameBgm;
+                break;
+            case BgmTrack.CIRCUIT:
+                currentBGM = soundBank.circuitMinigameBgm;
+                break;
+            case BgmTrack.ART:
+                currentBGM = soundBank.artMinigameBgm;
+                break;
+            case BgmTrack.MATH:
+                currentBGM = soundBank.mathMinigameBgm;
+                break;
+            case BgmTrack.MAINMENU:
+                currentBGM = soundBank.mainMenuBgm;
+                break;
+            default:
+                currentBGM = soundBank.mainMenuBgm;
+                break;
+        }
+
+        musicSource.clip = currentBGM;
         musicSource.Play();
     }
 
     public void PlayButtonClick()
     {
-        instance.PlaySFX(buttonClick);
+        instance.PlaySFX(soundBank.buttonClick);
     }
 
     public void PlaySFX(AudioClip clip)
---
+++ STEAM Superheroes Game/Assets/Scripts/AudioManager/SoundData_SO.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public enum BgmTrack
+{
+    MAINMENU,
+    PREMINIGAME,
+    MINING,
+    PROGRAMMING,
+    CIRCUIT,
+    ART,
+    MATH
+}
+
+[CreateAssetMenu(fileName ="SoundDataObject", menuName = "Scriptable Objects/SoundDataObject")]
+public class SoundData_SO : ScriptableObject
+{
+    [Header("-------- Music --------")]
+    public AudioClip mainMenuBgm;
+    public AudioClip preMinigameBgm;
+    public AudioClip miningMinigameBgm;
+    public AudioClip programmingMinigameBgm;
+    public AudioClip circuitMinigameBgm;
+    public AudioClip artMinigameBgm;
+    public AudioClip mathMinigameBgm;
+
+
+    [Header("-------- Sounds --------")]
+
+    [Header("General")]
+    public AudioClip levelComplete;
+    public AudioClip buttonClick;
+
+    [Header("Art Minigame")]
+    public AudioClip shapeGrabbed;
+    public AudioClip shapeReleased;
+
+    [Header("Circuit Minigame")]
+    public AudioClip logicGateOptionsOpened;
+    public AudioClip logicGateOptionSelected;
+    public AudioClip logicGateEndSuccess;
+    public AudioClip logicGateEndFailure;
+
+    [Header("Math Minigame")]
+    public AudioClip paintSelected;
+    public AudioClip paintApplied;
+
+    [Header("Mining Minigame")]
+    public AudioClip clickHexagon;
+    public AudioClip mineralCollected;
+
+    [Header("Satellite Minigame")]
+    public AudioClip satelliteCollected;
+    public AudioClip droneMoving;
+}
